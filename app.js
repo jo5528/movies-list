9cfa43fac22b09fafe87ec5ea7cbef8c6bb5b114
Joanna Hsiao <felizcelia@gmail.com>
2024-05-27 15:15:49 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000 
-app.get('/' , (req, res)=>{
-res.send('express app for movies')
+app.get('/' , (req, res) => {
+ res.redirect('/movies')
 })
+app.get('/movies', (req, res) => {
+res.send('movies-list')
+})
+
+app.get('/movie/:id', (req, res) => {
+const id = req.params.id 
+res.send(`read movie: ${id}`)
+})
+
 app.listen(port, () => {
- console.log(`2nd express server is running on http://localhost:${port}`)
+ console.log(`3rd express server is running on http://localhost:${port}`)
 })
