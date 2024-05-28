57fda4c1e720ae612a64b8ae014d8af324af294a
Joanna Hsiao <felizcelia@gmail.com>
2024-05-28 21:35:26 +0800

    feat: add search function to listing

--- app.js
+++ app.js
@@ -13,7 +13,16 @@ app.get('/' , (req , res) => {
  res.redirect('/movies')
 })
 app.get('/movies', (req, res) => {
- res.render('index', { movies, BASE_IMG_URL })
+  const keyword = req.query.search?.trim()
+  const matchedMovies = keyword ? movies.filter((mv) =>
+    Object.values(mv).some((property) => {
+      if (typeof property === 'string') {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : movies
+  res.render('index', { movies: matchedMovies, BASE_IMG_URL, keyword })
 })
 
 app.get('/movie/:id', (req, res) => {
