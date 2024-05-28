6ece96954da94db99461b9eca9a49124f833ce2e
Joanna Hsiao <felizcelia@gmail.com>
2024-05-28 11:34:29 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000 
+const movies = require('./public/jsons/movies.json').results
+const BASE_IMG_URL = 'https://movie-list.alphacamp.io/posters/'
+
 app.engine('.hbs', engine({extname: '.hbs'}))
-//透過這個方法來定義要使用的樣板引擎，其中參數 .hbs 是這個樣板引擎的名稱。
 app.set('view engine', '.hbs')
-//透過這個方法告訴 Express 說要設定的 view engine 是 hbs (handlebars)。
 app.set('views', './views')
 app.use(express.static('public'))
-
-app.get('/' , (req, res) => {
+app.get('/' , (req , res) => {
  res.redirect('/movies')
 })
 app.get('/movies', (req, res) => {
-res.render('index')
+ res.render('index', { movies, BASE_IMG_URL })
 })
 
 app.get('/movie/:id', (req, res) => {
 const id = req.params.id 
-res.send(`read movie: ${id}`)
+const movie = movies.find((mv) => mv.id.toString() === id)
+res.render('detail', {movie, BASE_IMG_URL})
 })
 
 app.listen(port, () => {
